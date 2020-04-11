cad24478f5271dad0253176b5dae1201243cb2de
Milad Heydari <miladheydari2011@gmail.com>
2020-04-11 13:54:28 +0430

    init

---
+++ app/src/main/java/co/health/test/corona/screen/info/InfoActivity.kt
@@ -0,0 +1,13 @@
+package co.health.test.corona.screen.info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.health.test.corona.R
+
+class InfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
