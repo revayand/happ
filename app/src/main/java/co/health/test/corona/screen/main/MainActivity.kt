c3cd2fb6a726da49c3a581e82c8801b5a5b62b2e
Milad Heydari <miladheydari2011@gmail.com>
2020-04-12 13:19:23 +0430

    add some ui

--- app/src/main/java/co/health/test/corona/screen/main/MainActivity.kt
+++ app/src/main/java/co/health/test/corona/screen/main/MainActivity.kt
@@ -1,13 +1,39 @@
 package co.health.test.corona.screen.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import co.health.test.corona.R
+import co.health.test.corona.screen.main.home.HomeFragment
+import co.health.test.corona.screen.main.question.QuestionFragment
+import co.health.test.corona.screen.main.settings.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nav.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frame, HomeFragment.newInstance("", ""))
+
+            }
+            R.id.navigation_question -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frame, QuestionFragment.newInstance("", ""))
+            }
+            R.id.navigation_settings -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frame, SettingsFragment.newInstance("", ""))
+            }
+        }
+        return true
     }
 }
